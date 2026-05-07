aa19f4f3e520171ef9359005d06c671722add3e9
Rohith <Rohithkumar2196@gmail.com>
2026-05-07 19:37:41 +0530

    updates

--- BookStoreMERN/backend/index.js
+++ BookStoreMERN/backend/index.js
@@ -10,7 +10,10 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://bookstore-mu46.onrender.com'
+  ]
 }))
 app.use(express.json());
 app.use('/books',routers)
